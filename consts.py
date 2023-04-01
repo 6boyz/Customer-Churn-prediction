b1e55d25e52d6e44535151a445f98842861b977a
Artem Mirvoda <mirvodartem@gmail.com>
2023-04-01 17:58:21 +0500

    Added train test data split

--- consts.py
+++ consts.py
@@ -1,3 +1,6 @@
 PATH = "data\\wallet_urfu.parquet.gzip"
 RFM_PATH = "data\\rfm.parquet.gzip"
 MODEL_PATH = "model\\rfm.model.pkl"
+
+TRAIN_PATH = "data\\train.parquet.gzip"
+TEST_PATH = "data\\test.parquet.gzip"
---
+++ utils.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+def time_now():
+   return datetime.now().strftime("%H:%M:%S")
+
+def print_log(string):
+   print(f'{time_now()} {string}')
