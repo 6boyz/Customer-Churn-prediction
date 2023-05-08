fea532122f708d198cc8a5c0bb0639b20be2e5b5
Artem Mirvoda <mirvodartem@gmail.com>
2023-05-08 12:31:52 +0500

    Temp

--- consts.py
+++ consts.py
@@ -1,6 +1,7 @@
 PATH = "data\\wallet_urfu.parquet.gzip"
 RFM_PATH = "data\\rfm.parquet.gzip"
 MODEL_PATH = "model\\rfm.model.pkl"
+MODEL_PATH_180_DAYS = "model\\rfm.model.180.days.pkl"
 
 TRAIN_PATH = "data\\train.parquet.gzip"
 TEST_PATH = "data\\test.parquet.gzip"
